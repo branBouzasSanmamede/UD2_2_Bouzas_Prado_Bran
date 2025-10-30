2e197ec548bc9eaa70459d02f2dd466dcc9744f9
branBouzasSanmamede <bran.bouzas@iessanmamede.com>
2025-10-30 10:50:13 +0100

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace UD2_2_Bouzas_Prado_Bran
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
